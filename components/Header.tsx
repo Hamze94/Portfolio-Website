f70ca45731aff0529d69b057c1793c03e0c0c0fd
Hamze Mohamed <hamzemohamed9412@gmail.com>
2024-06-18 10:57:50 +0300

    header and desktop nav impletendted

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import { Button } from "./ui/button"
+import Nav from "./Nav"
+export default function Header() {
+    return (
+        <header className="py-8 xl:py-12 text-white p">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href='/'>
+                    <h1 className=" text-4xl  font-semibold">Hamze <span className=" text-accent">MO</span> </h1>
+                </Link>
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
+
+            </div>
+
+        </header>
+    )
+}
